ac316d0d03de52c3768bf1e0d16db4fcc89d7940
韦钊 <759935172@qq.com>
2020-07-14 15:07:48 +0800

    第一次测试

---
+++ textServise/textServise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  textServise
+//
+//  Created by yunxu2020 on 2020/7/14.
+//  Copyright © 2020 yunxu2020. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
